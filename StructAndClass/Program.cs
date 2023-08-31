d9ff0fc5a70a23bb889fcb43e762e613364d2ce7
Samaya17 <mughumova@gmail.com>
2023-08-31 17:40:31 +0400

    version 1

---
+++ StructAndClass/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Counter
+    {
+        private int current = 0;
+        private int min;
+        private int max;
+
+        public int Min
+        {
+            get { return min; }
+            set { min = value; }
+        }
+
+        public int Max
+        {
+            get { return max; }
+            set { max = value; }
+        }
+
+        public Counter()
+        {
+            min = 0;
+            max = 0;
+        }
+
+        public Counter(int min, int max)
+        {
+            this.min = min;
+            this.max = max;
+        }
+
+        public void Increment()
+        {
+            if (current < max)
+            {
+                current++;
+            }
+            else
+            {
+                current = min;
+            }
+        }
+
+        public void Decrement()
+        {
+            if (current > min)
+            {
+                current--;
+            }
+            else
+            {
+                current = max;
+            }
+        }
+
+        public int GetCurrent()
+        {
+            return current;
+        }
+    }
+}
